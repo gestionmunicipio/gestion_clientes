d6f45977abb34e16c82ac6caa2f37140ced36861
gestionmunicipio <86022128+gestionmunicipio@users.noreply.github.com>
2025-09-25 15:51:31 -0300

    Update wsgi.py
    Actualización usuario gestionmunicipio

--- gestion_clientes/wsgi.py
+++ gestion_clientes/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Agrega la ruta de tu proyecto
+path = '/home/gestionmunicipio/cliente_admin'  # ← AJUSTA con tu usuario
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cliente_admin.settings')
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_clientes.settings')
-
 application = get_wsgi_application()
